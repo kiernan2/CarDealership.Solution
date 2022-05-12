b62ebbc7a79328ecd5ded3ef72676e9d20157c91
Kiernan2 <kiernan1994@gmail.com>
2022-05-12 15:57:02 -0700

    End of day

---
+++ CarDealership/Models/Car.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace Dealership.Models
+{
+  public class Car
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+
+    public Car(string makeModel,int price,int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public static string MakeSound(string sound)
+    {
+      return "Our cars sound like " + sound;
+    }
+
+    public void SetPrice(int newPrice)
+    {
+      Price = newPrice;
+    }
+
+    public string GetMakeModel()
+    {
+      return MakeModel;
+    }
+
+    public int GetPrice()
+    {
+      return Price;
+    }
+
+    public int GetMiles()
+    {
+      return Miles;
+    }
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+  }
+}
